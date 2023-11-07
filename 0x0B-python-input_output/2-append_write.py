244a7aafb115d5a9dd4b8b8666b756c844a36d19
root <root@LAPTOP-A03NS7VR>
2023-11-07 20:33:12 +0200

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@
 def append_write(filename="", text=""):
     """this function apppends stuff"""
     with open(filename, 'a', encoding='utf-8') as f:
-        f.write(text)
+        return f.write(text)
