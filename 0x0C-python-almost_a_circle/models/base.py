b39f1fc6464a1314d1e1d2977756781ef3695cdb
elyton250 <nelysemathieu@gmail.com>
2023-11-11 16:13:50 +0200

    correct pcode

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,13 +28,14 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """it writes a json string represenattion of objects"""
-        filename = cls.__name__ + ".json"
+        filename = str(cls.__name__) + ".json"
         with open(filename, 'w', encoding='utf-8') as j_file:
             if list_objs is None:
                 empty = []
                 j_file.write("[]")
             else:
-                json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+                json_string = cls.to_json_string([
+                        obj.to_dictionary() for obj in list_objs])
                 j_file.write(json_string)
 
     @staticmethod
@@ -48,3 +49,21 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """this sets attributes"""
+        dummy = cls(1, 1)
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Square":
+                dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """loads the json file"""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, 'r', encoding='utf-8') as j_file:
+                list_str = j_file.read()
+                list_inst = Base.from_json_string(list_str)
+                return [cls.create(**dicts) for dicts in list_inst]
+        except IOError:
+            return []
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """this module definees a square"""
-
-
 from models.rectangle import Rectangle
+
+
 class Square(Rectangle):
     """this class inherits the recatngle and defines the square"""
     def __init__(self, size, x=0, y=0, id=None):
@@ -11,15 +11,18 @@ class Square(Rectangle):
     def __str__(self):
         """this returns represtantiomn of square"""
         return f"[sqaure] ({self.id}) {self.x}/{self.y} - {self.width}"
+
     @property
     def size(self):
         """this is getter for size"""
         return self.width
+
     @size.setter
     def size(self, value):
         """this is a setter for size and size is already checked"""
         self.width = value
         self.height = value
+
     def update(self, *args, **kwargs):
         """this method assigns attributes"""
         attributes = ['id', 'size', 'x', 'y']
@@ -27,12 +30,13 @@ class Square(Rectangle):
             for i in range(len(args)):
                 setattr(self, attributes[i], args[i])
         else:
-             for key, value in kwargs.items():
+            for key, value in kwargs.items():
                 setattr(self, key, value)
+
     def to_dictionary(self):
         """return a dict represantation of the square"""
-        return {
-            'id': self.id, 
-            'size': self.size, 
-            'x': self.x, 
-            'y': self.y}
+        idd = self.id
+        size = self.size
+        x = self.x
+        y = self.y
+        return {'id': idd, 'size': size, 'x': x, 'y': y}
