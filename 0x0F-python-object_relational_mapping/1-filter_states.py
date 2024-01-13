12c129db850283c6aebf5f3071656721c89223c5
elyton250 <nelysemathieu@gmail.com>
2024-01-13 17:41:10 +0200

    task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# htis is task 2
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY states.id")
+        rows = cur.fetchall()
+        for row in rows:
+            if row[1].startswith('N'):
+                print(row)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL error [%d]: [%s]" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("Mysql error: [%s]" % str(e))
+    finally:
+        if "cur" in locals() and cur is not None:
+            cur.close()
+        if "db" in locals() and db is not None:
+            db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+        i = len(sys.argv[4])
+        state = sys.argv[4]
+        pss_state = state[:i]
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name='{}'".format(pss_state))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL error [%d]: [%s]" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("Mysql error: [%s]" % str(e))
+    finally:
+        if "cur" in locals() and cur is not None:
+            cur.close()
+        if "db" in locals() and db is not None:
+            db.close()
