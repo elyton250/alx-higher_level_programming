21a46382fb278a75a0948ac29e8892744c212fc8
elyton250 <nelysemathieu@gmail.com>
2024-01-26 19:50:33 +0200

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this uses request to get header"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers["X-Request-Id"])
