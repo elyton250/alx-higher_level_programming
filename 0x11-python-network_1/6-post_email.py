94ddca0135d2ef0aff7f1fd8b3765095bacc5d83
elyton250 <nelysemathieu@gmail.com>
2024-01-26 20:52:23 +0200

    task 8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""this uses request to post"""
+"""This script sends a POST request to http://0.0.0.0:5000/search_user"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    value = {'email': email}
-    response = requests.post(url, data=value)
+    url = 'http://0.0.0.0:5000/search_user'
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
 
-    print(response.text)
+    data = {'q': q}
+
+    response = requests.post(url, data=data)
+
+    try:
+        json_response = response.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
