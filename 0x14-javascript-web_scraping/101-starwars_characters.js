64427c6d52de1c1e94d7b34be7e6175ed2755cde
elyton250 <nelysemathieu@gmail.com>
2024-02-21 02:03:11 +0200

    task 101 work

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printChars(characters, 0);
+
+request(url, function (err, res, body) {
+  if (!err) {
+    const chars = JSON.parse(body).characters;
+    printChars(chars, 0);
   }
 });
-// the function that recurses
-function printChars (characters, index) {
-  request(characters[index], function (error, body) {
-    if (!error) {
+
+function printChars(characters, index) {
+  request(characters[index], function (err, res, body) {
+    if (!err) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
         printChars(characters, index + 1);
